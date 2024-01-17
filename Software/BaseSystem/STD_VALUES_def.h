b4d6b6a1c561c4a104269cfb81c44627a4581419
Eduardo Heredia Gonzalez <EduardoHerediaGonzalez@gmail.com>
2024-01-17 13:04:29 -0600

    Header files PLATFORM_TYPES_def.h and STD_VALUES_def.h added to folder BaseSystem. Folder BaseSystem moved to folder Software.

---
+++ Software/BaseSystem/STD_VALUES_def.h
@@ -0,0 +1,35 @@
+/**
+ * Copyright © 2024 
+ *
+ * @file    STD_VALUES_def.h
+ * @note
+ *        ______________      
+ *        Eduardo Heredia Gonzalez
+ *        Diseñador           
+ *
+ * @brief   STD_VALUES_def 
+ * @version No oficial
+ *     
+ */
+
+#ifndef STD_VALUES_def_H_
+#define STD_VALUES_def_H_
+
+/* system headers */
+
+/* exported macros */
+#define STD_nZERO       (0)
+#define STD_nLOW        (0)
+#define STD_nOFF        (0)
+#define STD_nIDLE       (0)
+
+#define STD_nONE        (1)
+#define STD_nHIGH       (1)
+#define STD_nON         (1)
+#define STD_nACTIVE     (1)
+
+#define STD_nUNDEFINED  (2)
+
+/* exported types */
+
+#endif /* STD_VALUES_def_H_ */
