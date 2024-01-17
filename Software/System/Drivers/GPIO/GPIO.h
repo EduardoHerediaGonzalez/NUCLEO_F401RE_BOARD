092c21e7aa4f2763931ef311da33e1626f754127
Eduardo Heredia Gonzalez <EduardoHerediaGonzalez@gmail.com>
2024-01-17 13:08:58 -0600

    Source files package of the component GPIO added to the project.

---
+++ Software/System/Drivers/GPIO/GPIO.h
@@ -0,0 +1,29 @@
+/**
+ * Copyright © 2024 
+ *
+ * @file    GPIO.h
+ * @note
+ *        ______________      
+ *        Eduardo Heredia Gonzalez
+ *        Diseñador           
+ *
+ * @brief   GPIO 
+ * @version No oficial
+ *     
+ */
+
+#ifndef GPIO_H_
+#define GPIO_H_
+
+/* system headers */
+
+/* own headers */
+
+/*****************************************************************************/
+/* helper macros */
+
+/* exported variables */
+
+/* exported functions */
+
+#endif /* GPIO_H_ */
